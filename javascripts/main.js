fef9a3fb31d0c706fd0a7bb9498dd35ee394df06
marmarosi <marmarosi@logikum.hu>
2015-01-02 23:36:24 +0100

    Using relative URLs.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 // Show the required content.
 function content (url) {
-  $.get( '/contents/' + url + '.html', function( data ) {
+  $.get( 'contents/' + url + '.html', function( data ) {
     $( '#main-content' ).html( data );
   });
   return false;
