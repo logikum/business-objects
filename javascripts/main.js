86690b4be3eb76582a777643b29ce1dbecb541e3
Mármarosi József <marmarosi@logikum.hu>
2015-01-02 20:14:52 +0100

    Setup documentation frame.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
-console.log('This would be the main JS file.');
+// Show the required content.
+function content (url) {
+  $.get( '/contents/' + url + '.html', function( data ) {
+    $( '#main-content' ).html( data );
+  });
+  return false;
+}
