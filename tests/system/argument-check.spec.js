0ceddae71f0d6cb21683ce23299290940ac07f35
marmarosi <marmarosi@logikum.hu>
2015-11-28 22:12:41 +0100

    Adding argument check tests.

--- tests/system/argument-check.spec.js
+++ tests/system/argument-check.spec.js
@@ -173,4 +173,354 @@ describe('Argument checking function', function () {
   });
 
   //endregion
+
+  //region Generic
+
+  it('asDefined method works', function () {
+    function call1() { return Argument.check().asDefined(); }
+
+    var any01 = Argument.check(null).asDefined();
+    var any02 = Argument.check(false).asDefined();
+    var any03 = Argument.check(1).asDefined();
+    var any04 = Argument.check(-100.99).asDefined();
+    var any05 = Argument.check('').asDefined();
+    var any06 = Argument.check('Romeo and Juliet').asDefined();
+    var any07 = Argument.check([]).asDefined();
+    var any08 = Argument.check({}).asDefined();
+    var any09 = Argument.check(function() {}).asDefined();
+    var any10 = Argument.check(new Date()).asDefined();
+    var any11 = Argument.check(new RegExp('[0-9]+')).asDefined();
+
+    expect(call1).toThrow('The argument must be supplied.');
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+    expect(any04).toBeDefined();
+    expect(any05).toBeDefined();
+    expect(any06).toBeDefined();
+    expect(any07).toBeDefined();
+    expect(any08).toBeDefined();
+    expect(any09).toBeDefined();
+    expect(any10).toBeDefined();
+    expect(any11).toBeDefined();
+  });
+
+  it('hasValue method works', function () {
+    function call1() { return Argument.check().hasValue(); }
+    function call2() { return Argument.check(null).hasValue(); }
+
+    var any01 = Argument.check(false).hasValue();
+    var any02 = Argument.check(1).hasValue();
+    var any03 = Argument.check(-100.99).hasValue();
+    var any04 = Argument.check('').hasValue();
+    var any05 = Argument.check('Romeo and Juliet').hasValue();
+    var any06 = Argument.check([]).hasValue();
+    var any07 = Argument.check({}).hasValue();
+    var any08 = Argument.check(function() {}).hasValue();
+    var any09 = Argument.check(new Date()).hasValue();
+    var any10 = Argument.check(new RegExp('[0-9]+')).hasValue();
+
+    expect(call1).toThrow();
+    expect(call2).toThrow('The argument is required.');
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+    expect(any04).toBeDefined();
+    expect(any05).toBeDefined();
+    expect(any06).toBeDefined();
+    expect(any07).toBeDefined();
+    expect(any08).toBeDefined();
+    expect(any09).toBeDefined();
+    expect(any10).toBeDefined();
+  });
+
+  //endregion
+
+  //region String
+
+  it('asString method works', function () {
+    function call01() { return Argument.check().asString(); }
+    function call02() { return Argument.check(null).asString(); }
+    function call03() { return Argument.check(false).asString(); }
+    function call04() { return Argument.check(1).asString(); }
+    function call05() { return Argument.check(-100.99).asString(); }
+    function call06() { return Argument.check([]).asString(); }
+    function call07() { return Argument.check({}).asString(); }
+    function call08() { return Argument.check(function() {}).asString(); }
+    function call09() { return Argument.check(new Date()).asString(); }
+    function call10() { return Argument.check(new RegExp('[0-9]+')).asString(); }
+
+    var any01 = Argument.check('').asString();
+    var any02 = Argument.check('Romeo and Juliet').asString();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+    expect(call10).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+  });
+
+  it('asString (optional) method works', function () {
+    function call01() { return Argument.check(false).forOptional().asString(); }
+    function call02() { return Argument.check(1).forOptional().asString(); }
+    function call03() { return Argument.check(-100.99).forOptional().asString(); }
+    function call04() { return Argument.check([]).forOptional().asString(); }
+    function call05() { return Argument.check({}).forOptional().asString(); }
+    function call06() { return Argument.check(function() {}).forOptional().asString(); }
+    function call07() { return Argument.check(new Date()).forOptional().asString(); }
+    function call08() { return Argument.check(new RegExp('[0-9]+')).forOptional().asString(); }
+
+    var any01 = Argument.check().forOptional().asString();
+    var any02 = Argument.check(null).forOptional().asString();
+    var any03 = Argument.check('').forOptional().asString();
+    var any04 = Argument.check('Romeo and Juliet').forOptional().asString();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+    expect(any04).toBeDefined();
+  });
+
+  it('asString (mandatory) method works', function () {
+    function call01() { return Argument.check().forMandatory().asString(); }
+    function call02() { return Argument.check(null).forMandatory().asString(); }
+    function call03() { return Argument.check(false).forMandatory().asString(); }
+    function call04() { return Argument.check(1).forMandatory().asString(); }
+    function call05() { return Argument.check(-100.99).forMandatory().asString(); }
+    function call06() { return Argument.check([]).forMandatory().asString(); }
+    function call07() { return Argument.check({}).forMandatory().asString(); }
+    function call08() { return Argument.check(function() {}).forMandatory().asString(); }
+    function call09() { return Argument.check(new Date()).forMandatory().asString(); }
+    function call10() { return Argument.check(new RegExp('[0-9]+')).forMandatory().asString(); }
+    function call11() { return Argument.check('').forMandatory().asString(); }
+
+    var any01 = Argument.check('Romeo and Juliet').forMandatory().asString();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+    expect(call10).toThrow();
+    expect(call11).toThrow();
+
+    expect(any01).toBeDefined();
+  });
+
+  //endregion
+
+  //region Number
+
+  it('asNumber (optional) method works', function () {
+    function call01() { return Argument.check(false).forOptional().asNumber(); }
+    function call02() { return Argument.check('').forOptional().asNumber(); }
+    function call03() { return Argument.check('Romeo and Juliet').forOptional().asNumber(); }
+    function call04() { return Argument.check([]).forOptional().asNumber(); }
+    function call05() { return Argument.check({}).forOptional().asNumber(); }
+    function call06() { return Argument.check(function() {}).forOptional().asNumber(); }
+    function call07() { return Argument.check(new Date()).forOptional().asNumber(); }
+    function call08() { return Argument.check(new RegExp('[0-9]+')).forOptional().asNumber(); }
+
+    var any01 = Argument.check().forOptional().asNumber();
+    var any02 = Argument.check(null).forOptional().asNumber();
+    var any03 = Argument.check(1).forOptional().asNumber();
+    var any04 = Argument.check(-100.99).forOptional().asNumber();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+    expect(any04).toBeDefined();
+  });
+
+  it('asNumber (mandatory) method works', function () {
+    function call01() { return Argument.check().forMandatory().asNumber(); }
+    function call02() { return Argument.check(null).forMandatory().asNumber(); }
+    function call03() { return Argument.check(false).forMandatory().asNumber(); }
+    function call04() { return Argument.check('').forMandatory().asNumber(); }
+    function call05() { return Argument.check('Romeo and Juliet').forMandatory().asNumber(); }
+    function call06() { return Argument.check([]).forMandatory().asNumber(); }
+    function call07() { return Argument.check({}).forMandatory().asNumber(); }
+    function call08() { return Argument.check(function() {}).forMandatory().asNumber(); }
+    function call09() { return Argument.check(new Date()).forMandatory().asNumber(); }
+    function call10() { return Argument.check(new RegExp('[0-9]+')).forMandatory().asNumber(); }
+
+    var any01 = Argument.check(1).forMandatory().asNumber();
+    var any02 = Argument.check(-100.99).forMandatory().asNumber();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+    expect(call10).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+  });
+
+  //endregion
+
+  //region Integer
+
+  it('asInteger (optional) method works', function () {
+    function call01() { return Argument.check(false).forOptional().asInteger(); }
+    function call02() { return Argument.check(-100.99).forOptional().asInteger(); }
+    function call03() { return Argument.check('').forOptional().asInteger(); }
+    function call04() { return Argument.check('Romeo and Juliet').forOptional().asInteger(); }
+    function call05() { return Argument.check([]).forOptional().asInteger(); }
+    function call06() { return Argument.check({}).forOptional().asInteger(); }
+    function call07() { return Argument.check(function() {}).forOptional().asInteger(); }
+    function call08() { return Argument.check(new Date()).forOptional().asInteger(); }
+    function call09() { return Argument.check(new RegExp('[0-9]+')).forOptional().asInteger(); }
+
+    var any01 = Argument.check().forOptional().asInteger();
+    var any02 = Argument.check(null).forOptional().asInteger();
+    var any03 = Argument.check(1).forOptional().asInteger();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+  });
+
+  it('asInteger (mandatory) method works', function () {
+    function call01() { return Argument.check().forMandatory().asInteger(); }
+    function call02() { return Argument.check(null).forMandatory().asInteger(); }
+    function call03() { return Argument.check(false).forMandatory().asInteger(); }
+    function call04() { return Argument.check(-100.99).forMandatory().asInteger(); }
+    function call05() { return Argument.check('').forMandatory().asInteger(); }
+    function call06() { return Argument.check('Romeo and Juliet').forMandatory().asInteger(); }
+    function call07() { return Argument.check([]).forMandatory().asInteger(); }
+    function call08() { return Argument.check({}).forMandatory().asInteger(); }
+    function call09() { return Argument.check(function() {}).forMandatory().asInteger(); }
+    function call10() { return Argument.check(new Date()).forMandatory().asInteger(); }
+    function call11() { return Argument.check(new RegExp('[0-9]+')).forMandatory().asInteger(); }
+
+    var any01 = Argument.check(1).forMandatory().asInteger();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+    expect(call10).toThrow();
+    expect(call11).toThrow();
+
+    expect(any01).toBeDefined();
+  });
+
+  //endregion
+
+  //region Boolean
+
+  it('asBoolean (optional) method works', function () {
+    function call01() { return Argument.check(1).forOptional().asBoolean(); }
+    function call02() { return Argument.check(-100.99).forOptional().asBoolean(); }
+    function call03() { return Argument.check('').forOptional().asBoolean(); }
+    function call04() { return Argument.check('Romeo and Juliet').forOptional().asBoolean(); }
+    function call05() { return Argument.check([]).forOptional().asBoolean(); }
+    function call06() { return Argument.check({}).forOptional().asBoolean(); }
+    function call07() { return Argument.check(function() {}).forOptional().asBoolean(); }
+    function call08() { return Argument.check(new Date()).forOptional().asBoolean(); }
+    function call09() { return Argument.check(new RegExp('[0-9]+')).forOptional().asBoolean(); }
+
+    var any01 = Argument.check().forOptional().asBoolean();
+    var any02 = Argument.check(null).forOptional().asBoolean();
+    var any03 = Argument.check(true).forOptional().asBoolean();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+
+    expect(any01).toBeDefined();
+    expect(any02).toBeDefined();
+    expect(any03).toBeDefined();
+  });
+
+  it('asBoolean (mandatory) method works', function () {
+    function call01() { return Argument.check().forMandatory().asBoolean(); }
+    function call02() { return Argument.check(null).forMandatory().asBoolean(); }
+    function call03() { return Argument.check(1).forMandatory().asBoolean(); }
+    function call04() { return Argument.check(-100.99).forMandatory().asBoolean(); }
+    function call05() { return Argument.check('').forMandatory().asBoolean(); }
+    function call06() { return Argument.check('Romeo and Juliet').forMandatory().asBoolean(); }
+    function call07() { return Argument.check([]).forMandatory().asBoolean(); }
+    function call08() { return Argument.check({}).forMandatory().asBoolean(); }
+    function call09() { return Argument.check(function() {}).forMandatory().asBoolean(); }
+    function call10() { return Argument.check(new Date()).forMandatory().asBoolean(); }
+    function call11() { return Argument.check(new RegExp('[0-9]+')).forMandatory().asBoolean(); }
+
+    var any01 = Argument.check(true).forMandatory().asBoolean();
+
+    expect(call01).toThrow();
+    expect(call02).toThrow();
+    expect(call03).toThrow();
+    expect(call04).toThrow();
+    expect(call05).toThrow();
+    expect(call06).toThrow();
+    expect(call07).toThrow();
+    expect(call08).toThrow();
+    expect(call09).toThrow();
+    expect(call10).toThrow();
+    expect(call11).toThrow();
+
+    expect(any01).toBeDefined();
+  });
+
+  //endregion
 });
